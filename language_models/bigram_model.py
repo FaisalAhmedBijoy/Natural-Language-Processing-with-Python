57331162fb776a809242d084b05c9677f7efea23
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2022-08-17 00:32:47 +0600

    Add files via upload

--- language_models/bigram_model.py
+++ language_models/bigram_model.py
@@ -1,5 +1,6 @@
 import re
 import os
+import random
 from pprint import pprint
 from collections import Counter
 
@@ -42,8 +43,17 @@ def bigram_probabilities(words):
             p_bigrams[words[x]] = {}
             p_bigrams[words[x]][words[x + 1]] = 1
             num_bigrams += 1
-    pprint(p_bigrams)
+    # pprint(p_bigrams)
     return p_bigrams
+
+def generate_sequence(p_unigrams,p_bigrams,num_words=100,seed_word=None):
+    if seed_word is None:
+        seed_word = random.choices(list(p_unigrams.keys()), weights=list(p_unigrams.values()))[0]
+    seq = [seed_word]
+    print(seq)
+    for i in range(num_words):
+        seq.append(random.choices(list(p_bigrams[seq[-1]].keys()), weights=list(p_bigrams[seq[-1]].values()))[0])
+    return seq
 
 if __name__ == '__main__':
     corpus_text_path='carrol_alice.txt'
@@ -51,6 +61,9 @@ if __name__ == '__main__':
     words=cleanup_corpus_and_tokenization(corpus)
     p_unigrams=unigram_probabilities(words)
     p_bigrams=bigram_probabilities(words)
+    sequence=generate_sequence(p_unigrams,p_bigrams)
+    print(sequence)
+
 
 
 
